e945b2aaf7d0eda4870a14432ade29a0caed832e
corp.jes <corporate@pahiram.ph>
2021-02-22 23:46:19 +0800

    web-fontain

--- src/App.js
+++ src/App.js
@@ -1,56 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useState } from "react";
+import Preview from "./components/Preview";
+import Progress from "./components/Progress";
 
 function App() {
+  const textData =
+    "Nulla qui ad in nostrud proident ea. Laboris labore dolore sunt ut pariatur ex ea laborum. Laborum esse culpa ex culpa reprehenderit irure minim qui pariatur. Officia fugiat eu sint culpa. Quis ad dolore nulla eiusmod eiusmod anim consequat consectetur nisi. Eiusmod veniam ea culpa commodo laborum ut nulla consequat labore proident anim officia. Elit mollit reprehenderit laboris duis dolor pariatur laborum ad id aliqua excepteur ullamco non.";
+
+  const [textPreview, setTextPreview] = useState(textData);
+  const [textInput, setTextInput] = useState("");
+
+  const onRepeat = () => {
+    setTextInput("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="py-4 flex flex-col justify-center items-center">
+      <Progress />
+      <Preview textPreview={textPreview} textInput={textInput} />
+      <div className="w-1/2">
+        <textarea
+          value={textInput}
+          onChange={(e) => setTextInput(e.target.value)}
+          className="w-full border border-gray-400 rounded-lg shadow-lg text-xl my-4 p-5 focus:outline-none"
+          placeholder="Start typing...."
+        ></textarea>
+        <div className="flex justify-between">
+          <div className="">
+            <button
+              onClick={() => onRepeat()}
+              className="focus:outline-none hover:bg-gray-400 hover:text-white font-semibold border border-gray-400 rounded-lg shadow-lg py-2 px-4 "
+            >
+              Restart
+            </button>
+          </div>
+          <div className="">
+            <button
+              onClick={() => {}}
+              className="focus:outline-none hover:bg-gray-400 hover:text-white font-semibold border border-gray-400 rounded-lg shadow-lg py-2 px-4 "
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Progress.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Progress() {
+  return (
+    <div>
+      <h1>Progress Area</h1>
+      {/* <Progress hasStripe value={64} /> */}
+    </div>
+  );
+}
+
+export default Progress;
